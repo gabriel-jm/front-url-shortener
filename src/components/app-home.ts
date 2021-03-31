fa6bb71a6e1b6d3fe7e503cf09385c70703eac87
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-03-31 00:24:44 -0300

    feat: Redefining html and css of app-home component

--- src/components/app-home.ts
+++ src/components/app-home.ts
@@ -1,8 +1,58 @@
-class AppHome extends HTMLElement {
+import './app-title'
+import './app-footer'
+import { SuperElement } from '../lib/super-element'
+import { css } from '../lib/template-functions/css'
+import { html } from '../lib/template-functions/html'
+
+class AppHome extends SuperElement {
   constructor() {
     super()
+  }
 
-    this.innerHTML = 'App HOME!!!'
+  cssStyle() {
+    return css`
+      :host {
+        display: grid;
+        height: 100%;
+        grid-template-rows: 70px auto 60px;
+      }
+
+      div {
+        padding: 0 16px;
+        text-align: center;
+      }
+
+      .url-input {
+        width: max-content;
+        margin: auto;
+        text-align: left;
+      }
+      
+      .url-input input {
+        display: block;
+      }
+    `
+  }
+
+  render() {
+    return html`
+      <app-title />
+
+      <section>
+        <div>
+          <p>Enter the long URL and get a short one</p>
+        </div>
+
+        <div class="url-input">
+          <label>
+            <span>Url</span>
+            <input />
+          </label>
+        </div>
+      </section>
+
+      <app-footer />
+    `
   }
 }
 
