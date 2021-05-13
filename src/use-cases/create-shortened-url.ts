623095d38dde384f6a140d669653e989e7830dee
Gabriel-JM <gabriel.josemelo@hotmail.com>
2021-05-12 22:20:46 -0300

    fix: change shown error messages

--- src/use-cases/create-shortened-url.ts
+++ src/use-cases/create-shortened-url.ts
@@ -1,6 +1,6 @@
 import { urlShortenerService } from '../services/url-shortener-service'
 
-const urlValidationRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+const urlValidationRegex = /^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
 
 export async function createShortenedUrl(rawUrl: string) {
   const match = rawUrl.trim().match(urlValidationRegex)
@@ -8,7 +8,11 @@ export async function createShortenedUrl(rawUrl: string) {
   if(!match || match[0] !== rawUrl) {
     return {
       ok: false,
-      data: 'Insert an URL with a valid format'
+      data: 'Invalid URL format' + (
+        (!rawUrl.includes('http') && !rawUrl.includes('https'))
+          ? '.\nPlease add http:// or https://'
+          : ''
+      )
     }
   }
 
@@ -19,7 +23,7 @@ export async function createShortenedUrl(rawUrl: string) {
   if(!response.ok) {
     return {
       ok: false,
-      data: 'Shortened Url creation error'
+      data: response.data?.error || 'Shortened URL creation error'
     }
   }
 
